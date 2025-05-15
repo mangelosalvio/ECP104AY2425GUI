b5f74c69386b1e12fce0094799c38bb5c80640bc
mangelosalvio <msalvio.13@gmail.com>
2025-05-15 14:51:24 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame frame;
         JTextField firstNameField, lastNameField;
         JLabel firstNameLabel , lastNameLabel;
-        JButton submitButton;
+        JButton submitButton, sampleButton;
 
         Container container;
 
@@ -19,6 +19,10 @@ public class Main {
         lastNameField = new JTextField(20);
         submitButton = new JButton("Save");
 
+        submitButton.setFont(new Font("Arial",Font.PLAIN,5));
+
+        sampleButton = new JButton("Sample");
+
         container = frame.getContentPane();
 
         container.setLayout(new GridBagLayout());
@@ -26,12 +30,39 @@ public class Main {
 
         constraints.gridx = 0;
         constraints.gridy = 0;
+
         container.add(firstNameLabel, constraints);
 
         constraints.gridx = 1;
         constraints.gridy = 0;
         container.add(firstNameField, constraints);
 
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        container.add(lastNameLabel, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        container.add(lastNameField, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        container.add(firstNameLabel, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        container.add(submitButton, constraints);
+
+        constraints.gridx = 3;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 3;
+        constraints.fill = GridBagConstraints.BOTH;
+
+        container.add(sampleButton, constraints);
 
         frame.setVisible(true);
         frame.pack();
